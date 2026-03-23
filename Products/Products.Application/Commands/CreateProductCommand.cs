8da4a3178b7d0976e53d5354e0afb00ab02cdae4
kerolos <kero.gpt@gmail.com>
2026-03-23 20:18:05 +0200

    productService

---
+++ Products/Products.Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Products.Application.Responses;
+using Products.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Application.Commands
+{
+    public class CreateProductCommand:IRequest<ProductDTO>
+    {
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+        public decimal OldPrice { get; set; }
+        public decimal NewPrice { get; set; }
+        public IFormFile? Image { get; set; }
+        public string CreatedBy { get; set; }
+       
+
+    }
+}
---
+++ Products/Products.Infrastructure/Data/Contexts/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Infrastructure.Data.Contexts
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+
+            var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionBuilder.UseSqlServer("Data Source=DESKTOP-AAOESE8\\SQLEXPRESS;Initial Catalog=ProductsDB;Integrated Security=True;Trust Server Certificate=True");
+            return new AppDbContext(optionBuilder.Options);
+
+
+            
+        }
+    }
+}
